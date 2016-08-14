cbaa4ea9d91247d56835584c29c058120a6e1918
webn00b <weberben.r@gmail.com>
2016-08-14 22:22:00 +0100

    error - Uncaught ReferenceError: total is not defined

--- Week04/lesson08/homework/temp_converter/js/index.js
+++ Week04/lesson08/homework/temp_converter/js/index.js
@@ -1,6 +1,13 @@
-var f;
-var c = 20;
+
+document.querySelector("#convert").onclick = resultInFahrenheit;
+
+// store number in input number to javascript
+// onclick convert button log number in result as farenheit
+
+
+function resultInFahrenheit ()  {
+  total = total + 32;
+  document.querySelector("#result").innerHTML = total;
+}
 
 //Do stuff
-
-console.log(f);
