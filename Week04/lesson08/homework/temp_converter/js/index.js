3512e23a406784bd373a248c504ba78d2497f245
webn00b <weberben.r@gmail.com>
2016-08-15 23:54:47 +0100

    auto celsius to farenheit - no convert no clear button

--- Week04/lesson08/homework/temp_converter/js/index.js
+++ Week04/lesson08/homework/temp_converter/js/index.js
@@ -1,13 +1,18 @@
 
-document.querySelector("#convert").onclick = resultInFahrenheit;
-
 // store number in input number to javascript
 // onclick convert button log number in result as farenheit
+document.getElementById('clear').onclick = clearNumbers;
 
 
-function resultInFahrenheit ()  {
-  total = total + 32;
-  document.querySelector("#result").innerHTML = total;
+function convert(degree) {
+    if (degree === "C") {
+        F = document.getElementById("c").value * 9 / 5 + 32;
+        document.getElementById("f").value = Math.round(F);
+    } else {
+        C = (document.getElementById("f").value -32) * 5 / 9;
+        document.getElementById("c").value = Math.round(C);
+    }
 }
+
 
 //Do stuff
